589c913facb0eac7c7545168fc659f2bb5be7049
LXXYOU005 <LXXYOU005@COM049G>
2016-09-08 02:04:43 +0200

    Added a GridView to the delete user page to allow the admin to view the database details.

--- MPAS/Admin/DeleteUser.aspx.designer.cs
+++ MPAS/Admin/DeleteUser.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace MPAS.Admin {
     public partial class DeleteUser {
         
         /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
         /// StudentNumberDelete_Label control.
         /// </summary>
         /// <remarks>
